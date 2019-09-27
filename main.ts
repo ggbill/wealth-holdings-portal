551466c182d253f6a8c3ca3f5793709b56cef15a
Bill <bill@goinggone.io>
2019-09-27 12:57:55 +0100

    messing with procfile / start

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ server.on('error', (err) => {
 });
 
 server.on('listening', async () => {
-    console.info(`Listening on port ${PORT}`);
+    console.info(`Listening on this port ${PORT}`);
     mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lightning", {
         useNewUrlParser: true,
         useFindAndModify: false
