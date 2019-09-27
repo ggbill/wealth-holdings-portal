57f7e1591d1d1dcca248bbbd3e792581b6d25624
Bill <bill@goinggone.io>
2019-09-27 18:53:57 +0100

    messing with procfile / start

--- backend/app.ts
+++ backend/app.ts
@@ -27,10 +27,10 @@ app.use('/players', playersRouter);
 app.use('/teams', teamsRouter);
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static( 'client/build '));
+    app.use(express.static( '../frontend/build '));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'server.js'));
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'server.js'));
     })
 }
 
