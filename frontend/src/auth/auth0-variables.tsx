e251598b885086b3a4a73507de85b5c2b918a961
Bill <bill@goinggone.io>
2019-11-11 19:45:14 +0000

    production fixes

--- frontend/src/auth/auth0-variables.tsx
+++ frontend/src/auth/auth0-variables.tsx
@@ -1,6 +1,8 @@
+const url = process.env.PUBLIC_URL || "http://localhost:8080"
+
 export const AUTH_CONFIG = {
   domain: 'going-gone.eu.auth0.com',
   clientId: '3F4xNRjs00Q75dqR4NisVZ5PztJRV0kK',
-  callbackUrl: 'http://localhost:3000/callback',
-  redirect_url: 'http://localhost:3000'
+  callbackUrl: `${url}/callback`,
+  redirect_url: url
 }
