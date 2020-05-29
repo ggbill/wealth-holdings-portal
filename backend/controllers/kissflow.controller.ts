6befe8b1b8e4e6ae1a1a36aadd863e39614eea74
Bill <bill@goinggone.io>
2020-05-29 16:20:43 +0100

    close case model updates

--- backend/controllers/kissflow.controller.ts
+++ backend/controllers/kissflow.controller.ts
@@ -30,7 +30,7 @@ export namespace KissFlowController {
                 companyType: webhookBody.Company_Type,
                 isSimplyBizMember: webhookBody.Is_SimplyBiz_Member,
                 isCloseCase: webhookBody.Close_Case,
-                closeCaseReason: webhookBody.Reason,
+                closeCaseReason: webhookBody.Close_Case_Reason,
                 closeCaseDescription: webhookBody.Close_Case_Description,
                 isReEngage: webhookBody.ReEngage_In_Future,
                 reEngageDate: webhookBody.ReEngage_Date,
